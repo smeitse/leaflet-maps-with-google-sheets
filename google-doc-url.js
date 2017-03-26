b397e0df6c9290c1f9cc38b0dcefca2d41f72fe9
smeitse <smeitse@gmail.com>
2017-03-26 21:44:01 +0800

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1kg3LJzWU4mEF1di2bftthg1vbuIFh0V9M41PaHutNR0/edit#gid=0';
